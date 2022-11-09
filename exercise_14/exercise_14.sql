5974a66fb5983597fc0bd94632f23429591a84dc
Tyler Watson <tylerww91@gmail.com>
2022-11-09 15:52:51 -0800

    finishes exercise 14, passes test

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,10 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT
+	category.name, AVG(film.length)
+FROM
+	film_category
+    LEFT JOIN film on film.film_id = film_category.film_id
+    LEFT JOIN category on category.category_id = film_category.category_id
+GROUP BY
+	category.name;
